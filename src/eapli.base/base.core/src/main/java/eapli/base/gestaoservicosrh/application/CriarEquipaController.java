44578160f0dad5183b20e7c517e291dbf342f3ab
1190769 <1190769@isep.ipp.pt>
2021-04-29 10:41:41 +0100

    LEI2DI04-26 #close #comment Atualização de pastas

--- src/eapli.base/base.core/src/main/java/eapli/base/gestaoservicosrh/application/CriarEquipaController.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/gestaoservicosrh/application/CriarEquipaController.java
@@ -1,11 +1,10 @@
 package gestaoservicosrh.application;
 
-import eapli.base.clientusermanagement.repositories.ClientUserRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import gestaoservicosrh.repositories.EquipaRepository;
 
 public class CriarEquipaController {
 
     private final EquipaRepository repo = (EquipaRepository) PersistenceContext.repositories().clientUsers();
-
+    
 }
---
+++ src/eapli.base/base.core/src/main/java/eapli/base/gestaoservicosrh/domain/Equipa.java
@@ -0,0 +1,26 @@
+package gestaoservicosrh.domain;
+
+import eapli.framework.domain.model.DomainEntities;
+import eapli.framework.domain.model.DomainEntity;
+
+import javax.persistence.Version;
+
+public class Equipa {
+    @Version
+    private Long version;
+
+    public Equipa() {
+
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return DomainEntities.areEqual((DomainEntity<?>) this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return DomainEntities.hashCode((DomainEntity<?>) this);
+    }
+
+}
