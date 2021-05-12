576c4634d5e2c84ca553292f5bac062bad3b6877
1190800 <1190800@isep.ipp.pt>
2021-05-12 15:28:08 +0100

    LEI2DI04-131#close#comment System.out corrections

--- src/eapli.base/base.core/src/main/java/eapli/base/gestaoservicosrh/application/AdicionarColaboradorEquipaController.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/gestaoservicosrh/application/AdicionarColaboradorEquipaController.java
@@ -1,24 +1,15 @@
 package eapli.base.gestaoservicosrh.application;
 
 import eapli.base.clientusermanagement.domain.MecanographicNumber;
-import eapli.base.gestaoservicosrh.domain.Acronimo;
 import eapli.base.gestaoservicosrh.domain.CodigoUnico;
-import eapli.base.gestaoservicosrh.domain.Designacao;
 import eapli.base.gestaoservicosrh.repositories.ColaboradorRepository;
 import eapli.base.gestaoservicosrh.repositories.EquipaRepository;
-import eapli.base.gestaoservicosrh.repositories.TipoEquipaRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
-import eapli.base.usermanagement.domain.BaseRoles;
 import eapli.base.usermanagement.domain.Colaborador;
 import eapli.base.gestaoservicosrh.domain.Equipa;
 import eapli.framework.application.UseCaseController;
-import eapli.framework.infrastructure.authz.application.AuthorizationService;
-import eapli.framework.infrastructure.authz.application.AuthzRegistry;
-import eapli.framework.infrastructure.authz.application.exceptions.UnauthorizedException;
 
-import javax.persistence.NoResultException;
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.List;
 
 @UseCaseController
 public class AdicionarColaboradorEquipaController {
@@ -64,15 +55,11 @@ public class AdicionarColaboradorEquipaController {
         return colaboradorRepository.containsOfIdentity(id);
     }
 
-    public void showAll() {
-        System.out.println("Available Teams   =========================================================================\n");
-        for (Equipa e : this.equipaRepository.findAll()) {
-            System.out.println(e);
-        }
-        System.out.println("===========================================================================================\n");
-        System.out.println("Available Collaborators   =================================================================\n");
-        for (Colaborador c :this.colaboradorRepository.findAll()) {
-            System.out.println(c);
-        }
-        System.out.println("===========================================================================================\n");    }
+    public List<Equipa> showAllTeams() {
+        return this.equipaRepository.findAll();
+    }
+
+    public List<Colaborador> showAllCollaborators() {
+        return (List<Colaborador>) this.colaboradorRepository.findAll();
+    }
 }
--- src/eapli.base/base.core/src/main/java/eapli/base/gestaoservicosrh/application/RemoverColaboradorEquipaController.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/gestaoservicosrh/application/RemoverColaboradorEquipaController.java
@@ -12,6 +12,8 @@ import eapli.framework.application.UseCaseController;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 
+import java.util.List;
+
 @UseCaseController
 public class RemoverColaboradorEquipaController {
 
@@ -41,17 +43,11 @@ public class RemoverColaboradorEquipaController {
     public boolean existsColaborador(MecanographicNumber id){
         return colaboradorRepository.containsOfIdentity(id);
     }
+    public List<Equipa> showAllTeams() {
+        return this.equipaRepository.findAll();
+    }
 
-    public void showAll() {
-        System.out.println("Available Teams   =========================================================================\n");
-        for (Equipa e : this.equipaRepository.findAll()) {
-            System.out.println(e);
-        }
-        System.out.println("===========================================================================================\n");
-        System.out.println("Available Collaborators   =================================================================\n");
-        for (Colaborador c :this.colaboradorRepository.findAll()) {
-            System.out.println(c);
-        }
-        System.out.println("===========================================================================================\n");
+    public List<Colaborador> showAllCollaborators() {
+        return (List<Colaborador>) this.colaboradorRepository.findAll();
     }
 }
