5f894631d4ab01b3474a5ae68616996c8fba67c1
1190974 <1190974@isep.ipp.pt>
2021-05-18 14:42:35 +0100

    Revert "LEI2DI04-129 #close #comment Implementação"
    This reverts commit fd7a4fde12d5bcdc25de80db997c8cd943747485.

--- src/eapli.base/base.core/src/main/java/eapli/base/atividades/domain/AtividadeAprovacao.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/atividades/domain/AtividadeAprovacao.java
@@ -1,19 +1,25 @@
 package eapli.base.atividades.domain;
 
+import eapli.base.colaborador.domain.Colaborador;
 import eapli.base.colaborador.domain.Data;
 import eapli.base.criticidade.domain.Criticidade;
 
 import javax.persistence.Column;
+import javax.persistence.OneToOne;
 
 public class AtividadeAprovacao extends Atividade{
+
+    @OneToOne
+    private Colaborador colab;
 
     @Column(name = "DECISAO")
     private Decisao decisao;
 
     private Comentario comentario;
 
-    public AtividadeAprovacao(Criticidade criticidade, Prioridade prioridade, Data dataLimite, EstadoAtividade estadoAtividade, Decisao decisao, Comentario comentario) {
+    public AtividadeAprovacao(Criticidade criticidade, Prioridade prioridade, Data dataLimite, EstadoAtividade estadoAtividade, Colaborador colab, Decisao decisao, Comentario comentario) {
         super(criticidade, prioridade, dataLimite, estadoAtividade);
+        this.colab = colab;
         this.decisao = decisao;
         this.comentario = comentario;
     }
--- src/eapli.base/base.core/src/main/java/eapli/base/atividades/domain/AtividadeManual.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/atividades/domain/AtividadeManual.java
@@ -1,12 +1,13 @@
 package eapli.base.atividades.domain;
 
+import eapli.base.colaborador.domain.Colaborador;
 import eapli.base.colaborador.domain.Data;
 import eapli.base.criticidade.domain.Criticidade;
 
 public class AtividadeManual extends AtividadeAprovacao{
 
-    public AtividadeManual(Criticidade criticidade, Prioridade prioridade, Data dataLimite, EstadoAtividade estadoAtividade, /*Colaborador colab, */Decisao decisao, Comentario comentario) {
-        super(criticidade, prioridade, dataLimite, estadoAtividade, decisao, comentario);
+    public AtividadeManual(Criticidade criticidade, Prioridade prioridade, Data dataLimite, EstadoAtividade estadoAtividade, Colaborador colab, Decisao decisao, Comentario comentario) {
+        super(criticidade, prioridade, dataLimite, estadoAtividade, colab, decisao, comentario);
     }
 
 }
