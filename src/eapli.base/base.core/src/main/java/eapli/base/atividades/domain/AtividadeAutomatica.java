5f281b95a70abb13179803811e0a1b642ed4a7cd
1190429 <1190429@isep.ipp.pt>
2021-05-24 17:59:24 +0100

    LEI2DI04-134 #close #comment US1006 Development

--- src/eapli.base/base.core/src/main/java/eapli/base/atividades/domain/AtividadeAutomatica.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/atividades/domain/AtividadeAutomatica.java
@@ -2,10 +2,7 @@ package eapli.base.atividades.domain;
 
 import eapli.framework.validations.Preconditions;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Calendar;
 
 @Entity
@@ -14,6 +11,9 @@ public class AtividadeAutomatica extends Atividade {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(name= "SCRIPT")
+    private Script script;
 
     public AtividadeAutomatica(Calendar dataLimite, EstadoAtividade estadoAtividade, TipoAtividade tipoAtividade) {
         super(dataLimite, estadoAtividade, tipoAtividade);
