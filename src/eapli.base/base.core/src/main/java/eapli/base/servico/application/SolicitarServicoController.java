9103cee7f1374c1ecaea1c7ff8ca6d988139372b
Tiago_1191098 <1191098@isep.ipp.pt>
2021-05-25 19:58:47 +0100

    LEI2DI04-88 US4001 #close #comment Continuação

--- src/eapli.base/base.core/src/main/java/eapli/base/servico/application/SolicitarServicoController.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/servico/application/SolicitarServicoController.java
@@ -22,10 +22,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.NoResultException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.*;
 
 @UseCaseController
 public class SolicitarServicoController {
+
+    static InetAddress serverIP;
+    static Socket sock;
 
     private AuthorizationService authz = AuthzRegistry.authorizationService();
     private final ColaboradorRepository repository = PersistenceContext.repositories().colaborador();
@@ -43,46 +52,45 @@ public class SolicitarServicoController {
 
     private Servico servico;
 
-    public List<Catalogo> displayAvailableCatalogos(){
+    public List<Catalogo> displayAvailableCatalogos() {
         List<Catalogo> catalogosDisponiveis = new ArrayList<>();
-        try{
+        try {
             Colaborador loggedColaborador = repository.findEmailColaborador(loggedUser.email());
             ArrayList<Equipa> associatedTeams = (ArrayList<Equipa>) repository.findAssociatedTeams(loggedColaborador.identity());
             for (Equipa e : associatedTeams) {
                 catalogosDisponiveis.addAll((Collection<? extends Catalogo>) catalogoRepository.findCatalogoEquipa(e.identity()));
             }
             catalogosAutorizados = (ArrayList<Catalogo>) catalogosDisponiveis;
-        }catch (Exception e){
+        } catch (Exception e) {
             LOGGER.error("Unexpected error");
-       }
+        }
         return catalogosDisponiveis;
     }
 
-    public Iterable<Servico> getServicosCatalogo(long idCatalogo){
+    public Iterable<Servico> getServicosCatalogo(long idCatalogo) {
         try {
             Catalogo c = catalogoRepository.findById(idCatalogo);
-            if(catalogosAutorizados.contains(c)){
+            if (catalogosAutorizados.contains(c)) {
                 return servicoRepository.findServicosDoCatalogo(idCatalogo);
-            }
-            else{
+            } else {
                 LOGGER.error("No authorization to access this catalog");
                 return null;
             }
-        }catch (NoResultException e){
+        } catch (NoResultException e) {
             LOGGER.error("Not found");
             return null;
         }
     }
 
-    public boolean efetuarPedido(UrgenciaPedido urgencia, Date dataLimiteRes){
+    public boolean efetuarPedido(UrgenciaPedido urgencia, Date dataLimiteRes) {
         //try{
-            Criticidade criticidade = servicoRepository.getCriticidade(servico.identity());
+        Criticidade criticidade = servicoRepository.getCriticidade(servico.identity());
         System.out.println(criticidade);
-            Colaborador colab = colaboradorRepository.findEmailColaborador(loggedUser.email());
-            Pedido pedido = new Pedido(colab,Calendar.getInstance().getTime(),servico,criticidade,urgencia,dataLimiteRes);
-            pedidoRepository.save(pedido);
-            return true;
-    //    }
+        Colaborador colab = colaboradorRepository.findEmailColaborador(loggedUser.email());
+        Pedido pedido = new Pedido(colab, Calendar.getInstance().getTime(), servico, criticidade, urgencia, dataLimiteRes);
+        pedidoRepository.save(pedido);
+        return true;
+        //    }
         //catch (Exception e){
         //    LOGGER.error("Something went wrong");
         //    return false;
@@ -92,18 +100,86 @@ public class SolicitarServicoController {
 
     public boolean preencherFormulario(String idServico) {
         //try{
-            servico = servicoRepository.ofIdentity(new CodigoUnico(idServico)).orElse(null);
-            if (servico != null) {
-                //Formulario formulario = servicoRepository.getAssociatedFormulario(idServico);
-                //formularioRepository.save(formulario);
-                return true;
-            }
-            return false;
+        servico = servicoRepository.ofIdentity(new CodigoUnico(idServico)).orElse(null);
+        if (servico != null) {
+            //Formulario formulario = servicoRepository.getAssociatedFormulario(idServico);
+            //formularioRepository.save(formulario);
+            return true;
+        }
+        return false;
         //}
         //catch (Exception e){
-          //  LOGGER.error("Something went wrong");
-            //return false;
-       // }
+        //  LOGGER.error("Something went wrong");
+        //return false;
+        // }
 
     }
+
+    public void doConnection(Pedido pedido) throws IOException, InterruptedException {
+        byte[] data = new byte[300];
+        String frase;
+        try {
+            serverIP = InetAddress.getByName("endereçoIp");
+        } catch (UnknownHostException ex) {
+            System.out.println("Invalid server: " + "endereçoIp");
+            System.exit(1);
+        }
+
+        try {
+            sock = new Socket(serverIP, 32507);
+        } catch (IOException ex) {
+            System.out.println("Failed to connect.");
+            System.exit(1);
+        }
+        DataOutputStream sOut = new DataOutputStream(sock.getOutputStream());
+        System.out.println("Connected to server");
+        //Thread serverConn = new Thread(new TcpChatCliConn(sock));
+        //serverConn.start();
+
+        //while(true) { // read messages from the console and send them to the server
+        frase = pedido.servico().identity().toString();
+        // if(frase.compareTo("exit")==0)
+        //{ sOut.write(0); break;}
+        data = frase.getBytes();
+        //  sOut.write((byte)frase.length());
+        //sOut.write(data,0,(byte)frase.length());
+        sOut.write((byte) 0);
+        sOut.write((byte) 3);
+        sOut.write((byte) frase.length());
+        sOut.write(data, 3, (byte) frase.length());
+        //}
+
+        // serverConn.join();
+        sock.close();
+    }
+
 }
+
+class TcpChatCliConn implements Runnable {
+    private Socket s;
+    private DataInputStream sIn;
+
+    public TcpChatCliConn(Socket tcp_s) {
+        s = tcp_s;
+    }
+
+    public void run() {
+        int nChars;
+        byte[] data = new byte[300];
+        String frase;
+
+        try {
+            sIn = new DataInputStream(s.getInputStream());
+            while (true) {
+                nChars = sIn.read();
+                if (nChars == 0) break;
+                sIn.read(data, 0, nChars);
+                frase = new String(data, 0, nChars);
+                System.out.println(frase);
+            }
+        } catch (IOException ex) {
+            System.out.println("Client disconnected.");
+        }
+    }
+}
+
