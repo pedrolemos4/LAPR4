ac6406778865660268a43ecda4aadded7dfda425
1190429 <1190429@isep.ipp.pt>
2021-06-09 14:15:15 +0100

    LEI2DI04-164 #close #comment US3023 Implementação da US

---
+++ src/eapli.base/base.core/src/main/java/eapli/base/validacoes/valida/ValidaForm.java
@@ -0,0 +1,28 @@
+package eapli.base.validacoes.valida;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ValidaForm {
+
+    public boolean validaForm(File file) {
+        //FileInputStream file = new FileInputStream(new File("testFormulario.txt"));
+        try {
+            FileInputStream file1 = new FileInputStream(file);
+            validaFormLexer lexer = new validaFormLexer((new ANTLRInputStream(file1)));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            validaFormParser parser = new validaFormParser(tokens);
+            ParseTree tree = parser.regra();
+            EvalVisitorValidaForm eval = new EvalVisitorValidaForm();
+            eval.visit(tree);
+        } catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+}
