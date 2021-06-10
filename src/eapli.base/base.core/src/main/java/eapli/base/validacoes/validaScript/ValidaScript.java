df909a57a99762e35495e358eed7a52eecfb1c8c
1190769 <1190769@isep.ipp.pt>
2021-06-10 10:42:04 +0100

    LEI2DI04-200 #close #comment Correção do método

--- src/eapli.base/base.core/src/main/java/eapli/base/validacoes/validaScript/ValidaScript.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/validacoes/validaScript/ValidaScript.java
@@ -1,21 +1,25 @@
 package eapli.base.validacoes.validaScript;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.File;
+import java.io.FileInputStream;
+
 public class ValidaScript {
-    /*
     public boolean validaScript(File script) {
         try {
             FileInputStream file = new FileInputStream(script);
-            validarAtividadeLexer lexer = new validarAtividadeLexer((new ANTLRInputStream(file)));
+            validarScriptLexer lexer = new validarScriptLexer((new ANTLRInputStream(file)));
             CommonTokenStream tokens = new CommonTokenStream(lexer);
-            validarAtividadeParser parser = new validarAtividadeParser(tokens);
+            validarScriptParser parser = new validarScriptParser(tokens);
             ParseTree tree = parser.regra();
-            EvalVisitorValidarAtividade eval = new EvalVisitorValidarAtividade();
+            EvalVisitorValidarScript eval = new EvalVisitorValidarScript();
             eval.visit(tree);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
-
-     */
 }
