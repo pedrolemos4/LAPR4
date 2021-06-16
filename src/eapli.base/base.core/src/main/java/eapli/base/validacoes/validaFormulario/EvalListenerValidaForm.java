31c0c9f9deb142e464373ad9097a9217aa24515b
Tiago_1191098 <1191098@isep.ipp.pt>
2021-06-16 10:04:02 +0100

    LEI2DI04-217 #close #comment Validações de formulário

--- src/eapli.base/base.core/src/main/java/eapli/base/validacoes/validaFormulario/EvalListenerValidaForm.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/validacoes/validaFormulario/EvalListenerValidaForm.java
@@ -1,44 +1,40 @@
 package eapli.base.validacoes.validaFormulario;
 
-import java.util.Stack;
-
 public class EvalListenerValidaForm extends validaFormBaseListener {
-
-    Stack<String> stack = new Stack<>();
 
     @Override
     public void enterValido(validaFormParser.ValidoContext ctx) {
-        System.out.println("Formulário válido");
+        System.out.println("Formulário válido.");
     }
 
     @Override
     public void exitValido(validaFormParser.ValidoContext ctx) {
-        System.out.println("Formulário válido");
+        System.out.println("Formulário válido.");
     }
 
     @Override
     public void enterInvalido(validaFormParser.InvalidoContext ctx) {
-        System.out.println("Formulário inválido");
+        System.out.println("Formulário inválido.");
     }
 
     @Override
     public void exitInvalido(validaFormParser.InvalidoContext ctx) {
-        System.out.println("Formulário inválido");
+        System.out.println("Formulário inválido.");
     }
 
     @Override
     public void enterVazio(validaFormParser.VazioContext ctx) {
-        System.out.println("Campo Vazio");
+        System.out.println("Campo Vazio.");
     }
 
     @Override
     public void exitVazio(validaFormParser.VazioContext ctx) {
-        System.out.println("Campo Vazio");
+        System.out.println("Campo Vazio.");
     }
 
     @Override
     public void enterInvalido2(validaFormParser.Invalido2Context ctx) {
-
+        System.out.println("Atributo incompleto.");
     }
 
     /**
@@ -48,6 +44,7 @@ public class EvalListenerValidaForm extends validaFormBaseListener {
      */
     @Override
     public void exitInvalido2(validaFormParser.Invalido2Context ctx) {
+        System.out.println("Atributo incompleto.");
     }
 
     /**
@@ -57,6 +54,7 @@ public class EvalListenerValidaForm extends validaFormBaseListener {
      */
     @Override
     public void enterVazio1(validaFormParser.Vazio1Context ctx) {
+        System.out.println("Campo Vazio.");
     }
 
     /**
@@ -66,6 +64,7 @@ public class EvalListenerValidaForm extends validaFormBaseListener {
      */
     @Override
     public void exitVazio1(validaFormParser.Vazio1Context ctx) {
+        System.out.println("Campo Vazio.");
     }
 
     /**
@@ -75,14 +74,31 @@ public class EvalListenerValidaForm extends validaFormBaseListener {
      */
     @Override
     public void enterValido2(validaFormParser.Valido2Context ctx) {
+        switch (ctx.tp.getText()) {
+            case "INTEGER":
+                if (!ctx.var.getText().matches("[0-9]|[1-9][0-9]+")) {
+                    System.out.println("Nome de variavel nao tem formato Integer!");
+                }
+                ;
+            case "STRING":
+                if (!ctx.var.getText().matches("[A-Z][a-z]+")) {
+                    System.out.println("Nome de variavel nao tem formato String!");
+                }
+                ;
+            case "DATA":
+                if (!ctx.var.getText().matches("[0-9][0-9][0-9][0-9]'/'('0'[1-9]|'1'[0-2])'/'('0'[1-9]|[1-2][0-9]|'3'[0-1])")) {
+                    System.out.println("Nome de variavel nao tem formato Data!");
+                }
+        }
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override
-    public void exitValido2(validaFormParser.Valido2Context ctx) {
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+        @Override
+        public void exitValido2 (validaFormParser.Valido2Context ctx){
+            System.out.println("Atributo válido.");
+        }
     }
-}
