5fa72921f0eb4d093b37cca1b9e5e6e6143973e9
1190800 <1190800@isep.ipp.pt>
2021-06-16 15:02:10 +0100

    LEI2DI04-217#close#comment Listener Implementation

--- src/eapli.base/base.core/src/main/java/eapli/base/validacoes/validaFormulario/EvalListenerValidaForm.java
+++ src/eapli.base/base.core/src/main/java/eapli/base/validacoes/validaFormulario/EvalListenerValidaForm.java
@@ -1,5 +1,9 @@
 package eapli.base.validacoes.validaFormulario;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
 public class EvalListenerValidaForm extends validaFormBaseListener {
 
     @Override
@@ -79,16 +83,17 @@ public class EvalListenerValidaForm extends validaFormBaseListener {
                 if (!ctx.var.getText().matches("[0-9]|[1-9][0-9]+")) {
                     System.out.println("Nome de variavel nao tem formato Integer!");
                 }
-                ;
+                break;
             case "STRING":
                 if (!ctx.var.getText().matches("[A-Z][a-z]+")) {
                     System.out.println("Nome de variavel nao tem formato String!");
                 }
-                ;
+                break;
             case "DATA":
                 if (!ctx.var.getText().matches("[0-9][0-9][0-9][0-9]'/'('0'[1-9]|'1'[0-2])'/'('0'[1-9]|[1-2][0-9]|'3'[0-1])")) {
                     System.out.println("Nome de variavel nao tem formato Data!");
                 }
+                break;
         }
     }
 
@@ -101,4 +106,83 @@ public class EvalListenerValidaForm extends validaFormBaseListener {
         public void exitValido2 (validaFormParser.Valido2Context ctx){
             System.out.println("Atributo válido.");
         }
+
+///    ///
+
+        @Override
+        public void enterValidoString(validaFormParser.ValidoStringContext ctx) {
+            if (!ctx.getText().matches("[A-Z][a-z]+")) {
+                System.out.println("Nome de variavel nao tem formato String!");
+            }
+        }
+
+        @Override
+        public void exitValidoString(validaFormParser.ValidoStringContext ctx) { System.out.println("Atributo válido.");}
+        /**
+        * {@inheritDoc}
+        *
+        * <p>The default implementation does nothing.</p>
+        */
+        @Override
+        public void enterValidoInteger(validaFormParser.ValidoIntegerContext ctx) {
+            if (!ctx.getText().matches("[0-9]+")) {
+                System.out.println("Nome de variavel nao tem formato Integer!");
+            }
+        }
+        /**
+        * {@inheritDoc}
+        *
+        * <p>The default implementation does nothing.</p>
+        */
+        @Override
+        public void exitValidoInteger(validaFormParser.ValidoIntegerContext ctx) { System.out.println("Atributo válido.");}
+        /**
+        * {@inheritDoc}
+        *
+         * <p>The default implementation does nothing.</p>
+         */
+        @Override
+        public void enterValidoData(validaFormParser.ValidoDataContext ctx) {
+            if (!ctx.getText().matches("[0-9][0-9][0-9][0-9]'/'('0'[1-9]|'1'[0-2])'/'('0'[1-9]|[1-2][0-9]|'3'[0-1])")) {
+                System.out.println("Nome de variavel nao tem formato Data!");
+            }
+        }
+        /**
+        * {@inheritDoc}
+        *
+        * <p>The default implementation does nothing.</p>
+        */
+        @Override
+        public void exitValidoData(validaFormParser.ValidoDataContext ctx) { System.out.println("Atributo válido.");}
+
+        /**
+        * {@inheritDoc}
+        *
+        * <p>The default implementation does nothing.</p>
+        */
+        @Override
+        public void enterEveryRule(ParserRuleContext ctx) { }
+        /**
+        * {@inheritDoc}
+        *
+        * <p>The default implementation does nothing.</p>
+        */
+        @Override
+        public void exitEveryRule(ParserRuleContext ctx) {System.out.println("Atributo válido.");}
+        /**
+        * {@inheritDoc}
+        *
+        * <p>The default implementation does nothing.</p>
+        */
+        @Override
+        public void visitTerminal(TerminalNode node) { }
+        /**
+        * {@inheritDoc}
+        *
+        * <p>The default implementation does nothing.</p>
+        */
+        @Override
+        public void visitErrorNode(ErrorNode node) { }
+
+
     }
