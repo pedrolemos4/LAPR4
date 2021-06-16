16c5c5127df8c07c4b9493b78a3691c218c815f7
1190974 <1190974@isep.ipp.pt>
2021-06-16 11:40:21 +0100

    LEI2DI04-192 #close #comment Finalização da implementação do algoritmo de tempo médio

--- src/eapli.base/base.daemon.motor/src/main/java/base/daemon/motor/protocol/FluxoRequest.java
+++ src/eapli.base/base.daemon.motor/src/main/java/base/daemon/motor/protocol/FluxoRequest.java
@@ -3,7 +3,6 @@ package base.daemon.motor.protocol;
 import base.daemon.executor.algorithms.WorkloadBasedAlgorithm;
 import base.daemon.motor.algorithms.AlgoritmoTempoMedio;
 import base.daemon.motor.algorithms.FirstComeFirstServeAlgorithm;
-import eapli.base.AppSettings;
 import eapli.base.Application;
 import eapli.base.atividade.application.AplicacoesController;
 import eapli.base.atividade.domain.Atividade;
@@ -16,12 +15,9 @@ import eapli.base.pedido.domain.EstadoPedido;
 import eapli.base.pedido.domain.Pedido;
 import eapli.base.pedido.repositories.PedidoRepository;
 import eapli.base.servico.domain.Servico;
-import eapli.framework.infrastructure.authz.application.AuthorizationService;
-import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.DataInputStream;
@@ -51,8 +47,6 @@ public class FluxoRequest extends AplicacoesRequest {
 
     @Override
     public byte[] execute() {
-        boolean atividadeManual = false;
-        boolean atividadeAutomatica = false;
 
         try {
 
@@ -80,7 +74,6 @@ public class FluxoRequest extends AplicacoesRequest {
                     Pedido pedido = pedidoRepository.findPedido(id);
                     pedido.adicionaColaborador(escolhido,atividade);
                     pedidoRepository.save(pedido);
-                    System.out.println(pedido.toString());
 
                 } else if (atividade instanceof AtividadeManual && atividade.tipoAtividade().equals(TipoAtividade.REALIZACAO)) {
                     controller.updatePedido(id, EstadoPedido.EM_RESOLUCAO);
@@ -97,7 +90,7 @@ public class FluxoRequest extends AplicacoesRequest {
                     }
 
                     controller.updatePedido(id, EstadoPedido.EM_RESOLUCAO);
-                    //mandar para o executor
+
                     byte[] data = new byte[258];
 
                     SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
@@ -123,21 +116,11 @@ public class FluxoRequest extends AplicacoesRequest {
 
                     DataOutputStream sOut = new DataOutputStream(sock.getOutputStream());
 
-                    /*ArrayList<Thread> threads = new ArrayList<>();
-                    List<Atividade> atividades = controller.getAtividadesAuto();
-                    for (Atividade at : atividades){
-                        System.out.println("Connected to server");
-                        Thread serverConn = new Thread(new TcpChatCliConn(at, sOut));
-                        threads.add(serverConn);
-                        serverConn.start();
-                    }*/
-
                     LOGGER.info("Connected to server");
 
                     Thread serverConn = new Thread(new TcpChatCliConn(sock));
                     serverConn.start();
 
-                    //CodigoUnico cod = new CodigoUnico(id);
                     String caminhoScript = controller.findScriptServico(servico.identity());
                     byte[] idArray = caminhoScript.getBytes();
                     int size = idArray.length;
@@ -174,26 +157,9 @@ public class FluxoRequest extends AplicacoesRequest {
                     serverConn.join();
                     sock.close();
 
-                    /*for (Thread thread : threads) {
-                        try {
-                            //sOut.write(data); ????????????
-                            thread.join();
-                        } catch (InterruptedException ex) {
-                            Logger.getLogger(FluxoRequest.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }*/
-
                     controller.updatePedido(id, EstadoPedido.CONCLUIDO);
-
-                    //atividadeAutomatica=true;
                 }
             }
-            /*byte[] data = new byte[3];
-            data[0]=1;
-            data[1]=1;
-            data[2]=0;*/
-
-            // controller.saveServico(servico);
         } catch (final NumberFormatException e) {
             return buildBadRequest("Invalid servico id").getBytes();
         } catch (IOException | InterruptedException e1) {
@@ -228,60 +194,23 @@ class TcpChatCliConn implements Runnable {
     private static final Logger LOGGER = LogManager.getLogger(FluxoRequest.class);
 
     private Socket s;
-    //private Atividade a;
     private DataInputStream sIn;
-    //private DataOutputStream sout;
 
     public TcpChatCliConn(Socket tcp_s) {
         s = tcp_s;
     }
 
-    /*public TcpChatCliConn(Atividade atividade, DataOutputStream out) {
-        a = atividade;
-        sout = out;
-    }*/
-
-
     public void run() {
         byte[] data = new byte[258];
 
         try {
-            /*//String caminhoScript = controller.findScriptServico(servico.identity());
-            String caminhoScript = controller.findScriptAtividade(a.identity());
-            byte[] idArray = caminhoScript.getBytes();
-            int size = idArray.length;
-            data[2] = (byte) size;
-            data[0] = 0;
-            data[1] = 10;
-
-            int amount_of_times = size % 255;
-
-            while (amount_of_times > 1) {
-                byte[] info = new byte[255];
-                info[0] = 0;
-                info[1] = 10;
-                for (int k = 0; k < 255; k++) {
-                    info[k + 2] = idArray[k];
-                }
-                sout.write(info);
-                size -= 255;
-                amount_of_times--;
-            }
-
-            for (int i = 0; i < size; i++) {
-                data[i + 2] = idArray[i];
-            }
-*/
             sIn = new DataInputStream(s.getInputStream());
             sIn.read(data);
-            //LOGGER.trace("Received message:----\n{}\n----", data);
 
             if (data[1] == 1) {
                 LOGGER.info("Mensagem recebida com sucesso");
                 LOGGER.info("Fim da operação");
             }
-            //Logger.getLogger(FluxoRequest.class.getName()).log(Level.SEVERE, "Received message:----\n{}\n----", data);
-            //sout.write(data); // TALVEZ AQUI
         } catch (IOException ex) {
             LOGGER.info("Client disconnected.");
         }
